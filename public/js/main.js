8c38970b43cda6011fa6d105da05033f98308a93
Josh Taningco <64685600+jtaningco@users.noreply.github.com>
2020-12-06 20:48:00 +0800

    updated fonts

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 // Get modal element
-var modal = document.getElementById('speakerNameModal'); 
+var modal = document.getElementById('modalSpeakerName'); 
 
 // Get open modal button or div
 var modalOpen = document.getElementById('workshopSpeakerName');
@@ -20,3 +20,54 @@ function closeModal(e) {
 }
 
 // Repeat these steps for each speaker
+
+// Hipster: Frances To
+var modalFrancesTo = document.getElementById('modalFrancesTo');
+var workshopFrancesTo = document.getElementById('workshopFrancesTo')
+workshopFrancesTo.addEventListener('click', openFrancesTo);
+window.addEventListener('click', closeFrancesTo);
+
+// Functions
+function openFrancesTo() {
+    modalFrancesTo.style.display = 'flex';
+}
+
+function closeFrancesTo(e) {
+    if(e.target == modalFrancesTo) {
+        modalFrancesTo.style.display = 'none';
+    }
+}
+
+// Hustler: Joni Salang-Oy
+var modalJoniSalangOy = document.getElementById('modalJoniSalangOy');
+var workshopJoniSalangOy = document.getElementById('workshopJoniSalangOy');
+workshopJoniSalangOy.addEventListener('click', openJoniSalangOy);
+window.addEventListener('click', closeJoniSalangOy);
+
+// Functions
+function openJoniSalangOy() {
+    modalJoniSalangOy.style.display = 'flex';
+}
+
+function closeJoniSalangOy(e) {
+    if(e.target == modalJoniSalangOy) {
+        modalJoniSalangOy.style.display = 'none';
+    }
+}
+
+// Culmination: Richard Cruz
+var modalRichardCruz = document.getElementById('modalRichardCruz');
+var workshopRichardCruz = document.getElementById('workshopRichardCruz');
+workshopRichardCruz.addEventListener('click', openRichardCruz);
+window.addEventListener('click', closeRichardCruz);
+
+// Functions
+function openRichardCruz() {
+    modalRichardCruz.style.display = 'flex';
+}
+
+function closeRichardCruz(e) {
+    if(e.target == modalRichardCruz) {
+        modalRichardCruz.style.display = 'none';
+    }
+}
